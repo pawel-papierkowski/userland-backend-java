033ce7572b60b7a289b6b4e027f802437c2b7175
Mader Levap <mader.levap@gmail.com>
2026-04-08 21:35:08 +0200

    Okay, there is no point in using GraalVM and I do not want to resign from Flyway. We are back to Temurin, yay.

--- src/main/java/org/portfolio/userland/UserLandApplication.java
+++ src/main/java/org/portfolio/userland/UserLandApplication.java
@@ -1,6 +1,5 @@
 package org.portfolio.userland;
 
-import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,12 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * <p>Note that placement of @SpringBootApplication determines root package to be scanned, including subpackages.</p>
  */
 @SpringBootApplication
-@RegisterReflectionForBinding({ // To prevent GraalVM crashes.
-		org.flywaydb.database.postgresql.PostgreSQLConfigurationExtension.class,
-		org.flywaydb.core.internal.command.clean.CleanModeConfigurationExtension.class,
-		org.flywaydb.core.internal.configuration.extensions.DeployScriptFilenameConfigurationExtension.class,
-		org.flywaydb.core.internal.logging.log4j2.Log4j2LogCreator.class
-})
 public class UserLandApplication {
 	/**
 	 * Entry point.
