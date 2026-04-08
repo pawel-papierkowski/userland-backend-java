27908a78f1d571a9d73c026a95f609a0862e3fb3
Paweł Papierkowski <pawel.papierkowski@gmail.com>
2026-04-08 16:57:22 +0200

    Hopefully that should fix GraalVM issues with FlyWay.

--- src/main/java/org/portfolio/userland/UserLandApplication.java
+++ src/main/java/org/portfolio/userland/UserLandApplication.java
@@ -1,6 +1,5 @@
 package org.portfolio.userland;
 
-import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,8 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * <p>Note that placement of @SpringBootApplication determines root package to be scanned, including subpackages.</p>
  */
 @SpringBootApplication
-// This tells GraalVM to preserve the reflection metadata for Flyway's PostgreSQL extension.
-@RegisterReflectionForBinding(org.flywaydb.database.postgresql.PostgreSQLConfigurationExtension.class)
 public class UserLandApplication {
 	/**
 	 * Entry point.
