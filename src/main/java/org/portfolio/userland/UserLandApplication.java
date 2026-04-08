5b36440f9b0c6788c1d21071041974bdd1964bb1
Paweł Papierkowski <pawel.papierkowski@gmail.com>
2026-04-08 20:45:23 +0200

    We will really have to do it one by one, aren't we.

--- src/main/java/org/portfolio/userland/UserLandApplication.java
+++ src/main/java/org/portfolio/userland/UserLandApplication.java
@@ -9,9 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * <p>Note that placement of @SpringBootApplication determines root package to be scanned, including subpackages.</p>
  */
 @SpringBootApplication
-@RegisterReflectionForBinding({
+@RegisterReflectionForBinding({ // To prevent GraalVM crashes.
 		org.flywaydb.database.postgresql.PostgreSQLConfigurationExtension.class,
 		org.flywaydb.core.internal.command.clean.CleanModeConfigurationExtension.class,
+		org.flywaydb.core.internal.configuration.extensions.DeployScriptFilenameConfigurationExtension.class,
 		org.flywaydb.core.internal.logging.log4j2.Log4j2LogCreator.class
 })
 public class UserLandApplication {
