5d56c3195f4971ccd703b27db132fac253158beb
Mader Levap <mader.levap@gmail.com>
2026-04-08 19:13:23 +0200

    Fix for ByteBuddy issue.

--- src/main/java/org/portfolio/userland/UserLandApplication.java
+++ src/main/java/org/portfolio/userland/UserLandApplication.java
@@ -1,5 +1,6 @@
 package org.portfolio.userland;
 
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * <p>Note that placement of @SpringBootApplication determines root package to be scanned, including subpackages.</p>
  */
 @SpringBootApplication
+@RegisterReflectionForBinding({
+		org.hibernate.bytecode.internal.bytebuddy.BytecodeProviderImpl.class // To prevent config error. Spring should not use it anyway on GraalVM.
+})
 public class UserLandApplication {
 	/**
 	 * Entry point.
