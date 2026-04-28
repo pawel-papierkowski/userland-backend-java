085fe2e52725cc89a0e4f2004eea2cd71592ebcd
Paweł Papierkowski <pawel.papierkowski@gmail.com>
2026-04-28 21:56:41 +0200

    Modifications to AGENTS.md.

--- src/main/java/org/portfolio/userland/system/BaseService.java
+++ src/main/java/org/portfolio/userland/system/BaseService.java
@@ -10,21 +10,31 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationEventPublisher;
 
 /**
- * Base for all standard services.
+ * <p>Base for all standard services. No actual executable code should be here, just fields for stuff that is used
+ * literally everywhere as convenience. That prevents "God Class".</p>
+ * <p>From this class domain-specific base service classes are inherited. For example, <code>BaseService</code> ->
+ * <code>BaseUserService</code> -> <code>UserRegisterService</code>, <code>UserLoginService</code>,
+ * <code>UserPasswordService</code> etc.</p>
+ * <p>Note: not all services implement base service, only those that need it.</p>
  */
 public abstract class BaseService {
+  /** Generator of random tokens, UUIDs etc. */
   @Autowired
   protected SecurityGeneratorService securityGeneratorService;
-  @Autowired
-  protected ApplicationEventPublisher eventPublisher;
-
+  /** System configuration. */
   @Autowired
   protected ConfigService configService;
+  /** Permission checking. */
   @Autowired
   protected PermissionService permissionService;
 
+  /** Date & time. */
   @Autowired
   protected ClockService clockService;
+
+  /** Spring events. */
+  @Autowired
+  protected ApplicationEventPublisher eventPublisher;
 
   /** System profile. */
   @Value("${app.main.profile}")
